0072ffb46bfb8b3b99bc1d8f9c2924820a8b3594
Eric You <bapsajura@naver.com>
2018-05-29 18:49:30 -0700

    initial work

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import sys
 import collections, re
 
+#Read words from txt file by words
+#Ref: https://stackoverflow.com/questions/13259288/returning-a-list-of-words-after-reading-a-file-in-python
+
+def read_words(words_file):
+    return [word for line in open(words_file, 'r') for word in line.split()]
+
 if __name__ == '__main__':
     print "Train the data"
 
     #texts = ['Hello how are you im fine and you']
 
     #bags = [collections.Counter(re.findall(r'\w+',txt)) for txt in texts]
-    print bags[0]
+    #print bags[0]
+
+    training_data = read_words('trainingSet.txt')
+    print training_data
