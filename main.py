fdab8728bfe8f19283ab6706e7e6a7fdd5fbfb15
Eric You <bapsajura@naver.com>
2018-05-29 18:43:25 -0700

    initial upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+import collections, re
+
+if __name__ == '__main__':
+    print "Train the data"
+
+    #texts = ['Hello how are you im fine and you']
+
+    #bags = [collections.Counter(re.findall(r'\w+',txt)) for txt in texts]
+    print bags[0]
