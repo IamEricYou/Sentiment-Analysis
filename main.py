a9181aec23970c14e45e4b63c75bbedce5c899d8
bolanosf <bolanosf@oregonstate.edu>
2018-05-29 20:12:49 -0700

    feature vocab added

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def get_data(data):
 
     for line in training_data:
         lines = line.split() # splie the line by whitespace
-        read_line = [n for n in lines] 
+        read_line = [n for n in lines]
         bag_of_int.append(int(read_line[-1][0]))
         read_line = remove_int(read_line) #Remove int from the list
         temp_list = []
@@ -23,19 +23,42 @@ def get_data(data):
             temp_list.append(item)
         temp_list = [x.lower() for x in temp_list] #Uppercase -> lowercase
         bag_of_words.append(temp_list)
-    
+
     return bag_of_words, bag_of_int
 
-if __name__ == '__main__':
-    print "Train the data"
 
-    #texts = ['Hello how are you im fine and you']
-    #bags = [collections.Counter(re.findall(r'\w+',txt)) for txt in texts]
-    #print bags[0]
+if __name__ == '__main__':
+    sentences = [["wow","loved","the","place"], ["not","tasty","and", "the", "texture", "was", "just", "nasty"]]
+    classification = [1,0]
+
+    vocab = []
+    vocabLength = 0;
+    featureInclude = []
 
     training_data = open('trainingSet.txt','r')
-    
-    bag_of_words, bag_of_int = get_data(training_data) 
 
-    print bag_of_words[0] #Just for showing the list.
-    print bag_of_int[0]
+    sentences, classification = get_data(training_data)
+
+    # creates sorted vocabulary
+    for x in range(0,len(sentences)):
+        for y in range(0,len(sentences[x])):
+            if sentences[x][y] not in vocab:
+                vocab.append(sentences[x][y])
+    vocab = sorted(vocab)
+    vocabLength = len(vocab)
+    vocab.append("class Label")
+
+    # feature count array
+    for c in range(0,len(sentences)):
+        temp = []
+        for d in range(0,vocabLength):
+            if vocab[d] in sentences[c]:
+                temp.append(1)
+            else:
+                temp.append(0)
+        temp.append(classification[c])
+        featureInclude.append(temp)
+
+    print vocab
+    # for elem in featureInclude:
+    #     print elem
